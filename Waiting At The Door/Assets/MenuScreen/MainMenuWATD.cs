bda31ef8a18f736505877e0f7295572790c64994
olipat <opatterson99@gmail.com>
2025-02-25 18:56:07 -0500

    Game Update
    -updated the level design / coloring
    -added a placeholder start screen that works
    -add a placeholder memento "tennis ball"

---
+++ Waiting At The Door/Assets/MenuScreen/MainMenuWATD.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuWATD : MonoBehaviour
+{
+    public void StartGame(){
+        SceneManager.LoadScene("DENIAL");
+    }
+}
