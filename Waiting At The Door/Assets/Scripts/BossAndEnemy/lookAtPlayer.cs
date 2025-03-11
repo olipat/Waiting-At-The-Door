58fe3c337ef21ab4c80ca3f4b8feb557c9f7ef45
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2025-03-11 13:54:33 -0400

    enemy/boss look at to follow player
    code for an enemy or boss to rotate to constantly be looking at the player

---
+++ Waiting At The Door/Assets/Scripts/BossAndEnemy/lookAtPlayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class lookAtPlayer : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targ = target.transform.position;
+        targ.z = 0f;
+
+        Vector3 objectPos = transform.position;
+        targ.x = targ.x - objectPos.x;
+        targ.y = targ.y - objectPos.y;
+
+        float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
