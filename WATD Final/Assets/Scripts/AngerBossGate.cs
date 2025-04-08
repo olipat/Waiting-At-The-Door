2b92c31efa6706dd0854c5671e9dcf3c96f54c37
olipat <opatterson99@gmail.com>
2025-04-08 17:48:47 -0400

    Updatesssss
    - boss door sound and animation connected
    -addded some assets
    - a little testing

--- WATD Final/Assets/Scripts/AngerBossGate.cs
+++ WATD Final/Assets/Scripts/AngerBossGate.cs
@@ -1,11 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class BossDoor : MonoBehaviour
 {
     public GameObject doorClosed;
+    public GameObject doorHalf;
     public GameObject doorOpened;
     public string lockedMessage = "You need all bone keys!";
     public float messageDuration = 2f;
+    public float doorDelay = 2f;
+    public AudioClip doorSound;
+    private AudioSource audioS;
 
     private bool isOpen = false;
     private Collider2D closedDoorCollider;
@@ -13,10 +18,15 @@ public class BossDoor : MonoBehaviour
     private void Start()
     {
         if (doorClosed != null) doorClosed.SetActive(true);
+        if (doorHalf != null) doorHalf.SetActive(false); 
         if (doorOpened != null) doorOpened.SetActive(false);
 
         if (doorClosed != null)
             closedDoorCollider = doorClosed.GetComponent<Collider2D>();
+
+        audioS = GetComponent<AudioSource>(); 
+        if (audioS == null)
+            audioS = gameObject.AddComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -27,7 +37,7 @@ public class BossDoor : MonoBehaviour
 
         if (BoneKeyManager.Instance.HasAllKeys())
         {
-            OpenDoor();
+            StartCoroutine(OpenDoorSequence());
         }
         else
         {
@@ -35,12 +45,38 @@ public class BossDoor : MonoBehaviour
         }
     }
 
-    void OpenDoor()
+    IEnumerator OpenDoorSequence() 
     {
         isOpen = true;
+
+        if (closedDoorCollider != null)
+            closedDoorCollider.enabled = false; 
+
         if (doorClosed != null) doorClosed.SetActive(false);
-        if (doorOpened != null) doorOpened.SetActive(true);
-        
+
+        if (doorSound != null && audioS != null)
+        {
+            audioS.PlayOneShot(doorSound, 15f);
+        }
+
+        if (doorHalf != null)
+        {
+            doorHalf.SetActive(true); 
+            Debug.Log("Half door active!");
+        }
+
+        yield return new WaitForSeconds(doorDelay); 
+
+        if (doorHalf != null) 
+        {
+            doorHalf.SetActive(false);
+            Debug.Log("Half door deactivated!");
+        }
+        if (doorOpened != null) 
+        {
+            doorOpened.SetActive(true);
+            Debug.Log("Door opened!");
+        }
     }
 }
 
