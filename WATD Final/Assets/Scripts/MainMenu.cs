a96c0f9fd5e8092e463fbf9fd1cefa809c699127
AdithNG <adithnishanth@gmail.com>
2025-04-26 00:47:36 -0400

    Main Manu and Depression END OF DEMO updated

--- WATD Final/Assets/Scripts/MainMenu.cs
+++ WATD Final/Assets/Scripts/MainMenu.cs
@@ -19,6 +19,8 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        videoPlayer.Play();
+        videoPlayer.Pause();
         AudioManager.instance.PlayMenuMusic();
         //StartCoroutine(FadeLoop());
     }
