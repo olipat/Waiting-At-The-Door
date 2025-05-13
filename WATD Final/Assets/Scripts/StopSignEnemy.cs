92699812c728139c8cb520c4ed01a83adbe24f82
olipat <opatterson99@gmail.com>
2025-05-13 19:38:47 -0400

    stop sign guy done

--- WATD Final/Assets/Scripts/StopSignEnemy.cs
+++ WATD Final/Assets/Scripts/StopSignEnemy.cs
@@ -1,30 +1,32 @@
+using System.Collections;
 using UnityEngine;
+
 public class StopSignEnemy : MonoBehaviour
 {
     public Sprite someSprite;
     public float patrolSpeed;
     public float chaseSpeed;
     public float detectionRange;
-   // public float stopDistance;
     public Transform player;
+
     private Rigidbody2D enemyRB;
     public GameObject groundCheck;
     public GameObject wallCheck;
     public LayerMask groundLayer;
+
     public bool facingRight;
     public bool isGrounded;
     public bool isWall;
 
-    private bool isBlocking;
+    private bool isBlocking = false;
     private bool isStopping = false;
-    private float stopTime = 5f;
+    private float stopTime = 1f;
 
-    Animator animator;
-    //adding vars to handle basic bark ability
+    private Animator animator;
     private bool pushedBack = false;
     public float pushDuration = 1.5f;
 
-    private bool stop = false;
+    private bool playerInTrigger = false;
 
     void Start()
     {
@@ -37,70 +39,81 @@ public class StopSignEnemy : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, 0.2f, groundLayer);
         isWall = Physics2D.OverlapCircle(wallCheck.transform.position, 0.2f, groundLayer);
 
-        if (pushedBack || player == null) return; 
+        if (pushedBack || player == null || isStopping) return;
 
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
-        if ((distanceToPlayer < detectionRange) && !isBlocking)
+        if (distanceToPlayer < detectionRange)
         {
             isBlocking = true;
         }
-        if (isBlocking){
-            MoveToBlockPlayer();
+        else
+        {
+            isBlocking = false;
         }
-        else {
+
+        if (isBlocking)
+        {
+            ChasePlayer();
+        }
+        else
+        {
             Patrol();
         }
     }
 
     void Patrol()
     {
-        if (isBlocking) return; 
+        animator.SetBool("isWalking", true);
+        animator.SetBool("shouldStop", false);
 
-        //enemyRB.linearVelocity = new Vector2((facingRight ? 1 : -1) * patrolSpeed, enemyRB.linearVelocity.y);
         transform.Translate(Vector2.right * patrolSpeed * Time.deltaTime);
 
-        if ((!isGrounded || isWall) && facingRight)
-        {
-            Flip();
-        }
-        else if ((!isGrounded || isWall) && !facingRight)
+        if ((!isGrounded || isWall))
         {
             Flip();
         }
     }
 
-    void MoveToBlockPlayer()
+    void ChasePlayer()
     {
+        animator.SetBool("isWalking", true);
+        animator.SetBool("shouldStop", false);
+
         float direction = Mathf.Sign(player.position.x - transform.position.x);
-        float distance = Mathf.Abs(player.position.x - transform.position.x);
+        Vector2 targetPos = new Vector2(player.position.x, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, chaseSpeed * Time.deltaTime);
 
-
-        float stopThreshold = 0.5f;
-        float resumeThreshold = 0.7f;
-
-        if (distance > resumeThreshold)
-        {
-            animator.SetBool("shouldStop", false);
-            animator.SetBool("isWalking", true);
-
-            Vector2 targetPos = new Vector2(player.position.x, transform.position.y);
-            transform.position = Vector2.MoveTowards(transform.position, targetPos, chaseSpeed * Time.deltaTime);
-        }
-        else if (distance < stopThreshold && !isStopping)
-        {
-            StartCoroutine(StopAndWaitCoroutine());
-        }
-
-        // Face the player
         if ((direction > 0 && !facingRight) || (direction < 0 && facingRight))
         {
             Flip();
         }
     }
 
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
 
-    private System.Collections.IEnumerator StopAndWaitCoroutine()
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !isStopping)
+        {
+            playerInTrigger = true;
+            StartCoroutine(StopForDuration());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+        }
+    }
+
+    private IEnumerator StopForDuration()
     {
         isStopping = true;
         animator.SetBool("shouldStop", true);
@@ -108,72 +121,36 @@ public class StopSignEnemy : MonoBehaviour
 
         yield return new WaitForSeconds(stopTime);
 
-        float distanceAfterWait = Mathf.Abs(player.position.x - transform.position.x);
-        if (distanceAfterWait > 0.7f) 
-        {
-            animator.SetBool("shouldStop", false);
-            animator.SetBool("isWalking", true);
-        }
-
         isStopping = false;
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        isBlocking = distanceToPlayer < detectionRange;
     }
 
-    void Flip()
+    public void ApplyKnockback(Vector2 direction, float force)
     {
-        facingRight = !facingRight;
-        transform.Rotate(new Vector3(0, 180, 0));
+        StartCoroutine(StunCoroutine());
+
+        if (force > 0.1f)
+        {
+            enemyRB.AddForce(direction * force, ForceMode2D.Impulse);
+        }
+    }
+
+    private IEnumerator StunCoroutine()
+    {
+        pushedBack = true;
+        enemyRB.linearVelocity = Vector2.zero;
+        yield return new WaitForSeconds(pushDuration);
+        pushedBack = false;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, detectionRange);
-        Gizmos.color = Color.blue;
-       // Gizmos.DrawWireSphere(transform.position, stopDistance);
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(groundCheck.transform.position, 0.2f);
-        Gizmos.DrawWireSphere(wallCheck.transform.position, 0.2f);
-    }
-
-    //functions below added to handle bark ability being applied 
-    public void ApplyKnockback(Vector2 direction, float force)
-    {
-        StartCoroutine(StunCoroutine());
-
-        // now with the reduced bark force it'll stun stop sign guy no matter force
-        if (force > 0.1f) 
-        {
-            enemyRB.AddForce(direction * force, ForceMode2D.Impulse);
-        }
-    }
-
-    private System.Collections.IEnumerator StunCoroutine()
-    {
-        pushedBack = true;
-        enemyRB.linearVelocity = Vector2.zero; 
-
-        yield return new WaitForSeconds(1.5f); 
-
-        pushedBack = false;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.CompareTag("Player"))
-        {
-            stop = true;
-            animator.SetBool("shouldStop", true);
-            isBlocking = true;
-            this.GetComponent<SpriteRenderer>().sprite = someSprite;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if(collision.CompareTag("Player"))
-        {
-            isBlocking = false;
-            animator.SetBool("shouldStop", false);
-        }
+        if (groundCheck != null) Gizmos.DrawWireSphere(groundCheck.transform.position, 0.2f);
+        if (wallCheck != null) Gizmos.DrawWireSphere(wallCheck.transform.position, 0.2f);
     }
 }
